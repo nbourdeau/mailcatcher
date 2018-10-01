2f1fd219352f42017433aa9f3803a5c94f15dcbb
Nicolas Bourdeau <nbourdeau@diffusion.cc>
2018-10-01 13:58:23 -0400

    Beanstalk packaging

--- src/main/java/com/github/nbourdeau/mailcatcher/App.java
+++ src/main/java/com/github/nbourdeau/mailcatcher/App.java
@@ -3,6 +3,7 @@ package com.github.nbourdeau.mailcatcher;
 import com.dumbster.smtp.SimpleSmtpServer;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App  implements ApplicationRunner, DisposableBean {
 
+    @Value("${smtp.port}")
+    private Integer smtpPort;
     private SimpleSmtpServer smtpServer;
     private final MessageService messageService;
 
@@ -25,15 +28,7 @@ public class App  implements ApplicationRunner, DisposableBean {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (args.containsOption("help")) {
-            System.out.println("Usage: java -jar mailcatcher.jar [--port=<int port>]\n");
-            System.out.println("Default port is 25");
-            return;
-        }
-        int port = SimpleSmtpServer.DEFAULT_SMTP_PORT;
-        if (args.containsOption("port"))
-            port = Integer.parseInt(args.getOptionValues("port").get(0));
-        smtpServer = SimpleSmtpServer.start(port, messageService);
+        smtpServer = SimpleSmtpServer.start(smtpPort, messageService);
     }
 
     @Override
